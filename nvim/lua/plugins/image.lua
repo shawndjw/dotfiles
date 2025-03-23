e20e2d1ab08a350b360131e7157e699af5a7361b
Shawn Wilcox <shawn.wilcox@blood.ca>
2025-03-23 09:49:44 -0400

    chore: fixed path to assets

--- nvim/lua/plugins/image.lua
+++ nvim/lua/plugins/image.lua
@@ -3,8 +3,12 @@ return {
     "HakonHarnes/img-clip.nvim",
     event = "VeryLazy",
     opts = {
-      -- add options here
-      -- or leave it empty to use the default settings
+      default = {
+        dir_path = function()
+          return vim.fn.expand("%:p:h") .. "/assets/"
+        end,
+        file_name = "%Y-%m-%d-%H-%M-%S",
+      },
     },
     keys = {
       -- suggested keymap
@@ -15,7 +19,7 @@ return {
     "3rd/image.nvim",
     opts = {},
     rocks = {
-      hererocks = true,  -- recommended if you do not have global installation of Lua 5.1.
+      hererocks = true, -- recommended if you do not have global installation of Lua 5.1.
     },
   },
 }
