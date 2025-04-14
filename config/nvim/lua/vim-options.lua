bbf247c52cabe2e5d1902b3fb9ac79aac9daee82
Shawn Wilcox <shawn.wilcox@blood.ca>
2025-04-14 17:02:30 -0400

    chore: add keymaps for window splitting

--- config/nvim/lua/vim-options.lua
+++ config/nvim/lua/vim-options.lua
@@ -4,3 +4,5 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>s", "<cmd>split<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>v", "<cmd>vsplit<CR>", { noremap = true, silent = true })
