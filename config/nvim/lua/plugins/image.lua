ba073ad20bb67e2462baebc6734f78997a4c70ba
Shawn Wilcox <shawn.wilcox@blood.ca>
2025-10-16 16:49:39 -0400

    chore: re-enable diagram

---
+++ config/nvim/lua/plugins/image.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"3rd/image.nvim",
+		opts = {
+			processor = "magick_cli",
+		},
+		rocks = {
+			-- hererocks = true, -- recommended if you do not have global installation of Lua 5.1.
+		},
+	},
+}
